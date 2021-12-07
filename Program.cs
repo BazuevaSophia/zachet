cf5e99fdef4f8a83482aa2cf964d78f186a28c40
BazuevaSophia <93267487+BazuevaSophia@users.noreply.github.com>
2021-12-07 12:40:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace задание2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i;
+            Console.WriteLine("Enter a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("числа между a и b");
+            for (i = b - 1; i > a; i--)
+                Console.WriteLine(i);
+            Console.WriteLine("Количество чисел \n");
+            Console.WriteLine(b - a - 1);
+            Console.ReadKey();
+        }
+    }
+}
